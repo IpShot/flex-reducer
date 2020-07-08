70776fcd164d670bfc169e88d1d292367ba41986
IpShot <zhernosek.roman@gmail.com>
2020-07-08 17:49:19 +0500

    add jest config

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+const mainConfig = {
+  displayName: 'ReactDOM',
+  testMatch: ['<rootDir>/test/**/*.test.js'],
+  coverageDirectory: './coverage/',
+  collectCoverage: true
+}
+
+module.exports = {
+  projects: [mainConfig]
+}
