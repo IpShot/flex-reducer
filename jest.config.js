48df45e321982383b0a823db9df53797e71273dc
IpShot <zhernosek.roman@gmail.com>
2020-07-08 19:45:25 +0500

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,6 @@
-const mainConfig = {
+module.exports = {
   displayName: 'ReactDOM',
   testMatch: ['<rootDir>/test/**/*.test.js'],
   coverageDirectory: './coverage/',
   collectCoverage: true
-}
-
-module.exports = {
-  projects: [mainConfig]
 }
